73472f2e84c2303a680f2b30be9977bb4d781169
Jed Stout <jedstout@gmail.com>
2017-08-21 18:46:44 -0600

    git ignore folder

---
+++ Week2/Day1.rb
@@ -0,0 +1,22 @@
+Initialize Git Repository
+
+git init 
+
+
+touch passwords.txt
+
+
+Git Commands
+
+
+git status
+
+git add .
+
+git remove .
+
+git commit -m "Stuff and Stuff"
+
+git push origin master
+
+
